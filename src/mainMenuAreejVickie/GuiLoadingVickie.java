c28712df9df45767aafbbc142f3170c144dad8ab
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2018-01-16 14:30:50 -0500

    V

---
+++ src/mainMenuAreejVickie/GuiLoadingVickie.java
@@ -0,0 +1,14 @@
+package mainMenuAreejVickie;
+
+import guiTeacher.GUIApplication;
+
+public class GuiLoadingVickie extends GUIApplication{
+
+	public GuiLoadingVickie() {
+		
+		//3 screens stephKev
+		//2 screens JessJi
+		//2 or 3 screens AnnieJoanna
+	}
+
+}
