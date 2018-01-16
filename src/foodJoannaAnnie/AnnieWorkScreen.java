93ca9e69a0279e047355ec9a233f5e40baf399d1
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2018-01-16 15:29:10 -0500

    /

--- src/foodJoannaAnnie/AnnieWorkScreen.java
+++ src/foodJoannaAnnie/AnnieWorkScreen.java
@@ -1,30 +1,68 @@
 package foodJoannaAnnie;
 
-import java.util.List;
+import java.awt.Color;
+import java.util.*;
 
+import guiTeacher.components.*;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.ClickableScreen;
 
 public class AnnieWorkScreen extends ClickableScreen {
-
+	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+
+	public static AnnieFoodItem[] items;
+	public static String[] names;
+	public static double[] prices;
+
+	private JoannaOrders currentOrder;
+	private ArrayList<AnnieFoodItem> onScreen;
 	
-	private String[] names;
-	private double[] price;
-	private AnnieFoodItem[] ingredients;
-	private Anni
+	private int trashCount;
 
 	public AnnieWorkScreen(int width, int height) {
 		super(width, height);
-		names = {""
+		setBackground(new Color(180, 230, 230));
+		
 	}
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-		
+		Button done = new Button(getWidth() - 100, getHeight() - 100, 100, 50, "done", new Action() {
+			
+			@Override
+			public void act() {
+				Testing.test.setScreen(new JoannaResultScreen(getWidth(), getHeight(), currentOrder, onScreen));
+			}
+		});
+		viewObjects.add(done);
+	}
+
+	public AnnieFoodItem[] getItems() {
+		return items;
+	}
+
+	public String[] getNames() {
+		return names;
+	}
+
+	public double[] getPrices() {
+		return prices;
+	}
+
+	public JoannaOrders getCurrentOrder() {
+		return currentOrder;
+	}
+
+	public ArrayList<AnnieFoodItem> getOnScreen() {
+		return onScreen;
+	}
+
+	public int getTrashCount() {
+		return trashCount;
 	}
 
 }
