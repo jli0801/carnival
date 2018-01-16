5d9f2758b24ad923d77c49553179643d49e900f1
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2018-01-16 14:37:58 -0500

    screen

--- src/mainMenuAreejVickie/MainScreenAreej.java
+++ src/mainMenuAreejVickie/MainScreenAreej.java
@@ -1,18 +1,66 @@
 package mainMenuAreejVickie;
 
-public class MainScreenAreej {
-//areej
-	public MainScreenAreej() {
-		// TODO Auto-generated constructor stub
+import java.awt.Color;
+import java.awt.Font;
+import java.io.File;
+import java.util.List;
+
+import guiTeacher.components.Action;
+import guiTeacher.components.Button;
+import guiTeacher.components.Graphic;
+import guiTeacher.components.StyledComponent;
+import guiTeacher.components.TextArea;
+import guiTeacher.interfaces.Visible;
+import guiTeacher.userInterfaces.FullFunctionScreen;
+import mainMenuAreejVickie.GuiLoadingVickie;
+
+public class MainScreenAreej extends FullFunctionScreen {
+
+	public MainScreenAreej(int width, int height) {
+		super(width, height);
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	public void initAllObjects(List<Visible> viewObjects) {
+		
+Graphic baymaxtree = new Graphic(0,0, getWidth(), getHeight(), "resources/baymaxtree.jpg");
+		
+		back = new Button(350, 580, 70, 30, "Page 1", Color.red, new Action() {
 
-}
-		/*
-		 * pic buttons
- 		*/
+			public void act() {
+				
+				GuiPart.part.setScreen(GuiPart.windowOne);
+
+			}
+		});
+		TextArea ta = new TextArea(140, 180, 330 ,400, "HAPPY NEW YEAR!");
+		 try {
+
+			 File fontFile = new File("resources//AdventPro-Medium.ttf");
+
+			 // File fontFile = new File("resources//DayRoman.ttf");
+
+			 Font font = Font.createFont(Font.TRUETYPE_FONT, fontFile);
+
+			 Font baseFont=font.deriveFont(50f);
+
+			 back.setFont(baseFont);
+
+			 } catch (Exception e) {
+
+			 e.printStackTrace();
+
+			 }
+		ta.setTextColor(Color.black);
+	
+		
+		viewObjects.add(baymaxtree);
+		viewObjects.add(back);
+		viewObjects.add(ta);
+		
+		 StyledComponent.setButtonOutline(true);
+		
+		
+	}
 
 }
