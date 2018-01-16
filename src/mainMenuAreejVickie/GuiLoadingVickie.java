b57a0246ffeeeac9c14b9613bc90a727d8b6866e
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2018-01-16 14:38:30 -0500

    V

--- src/mainMenuAreejVickie/GuiLoadingVickie.java
+++ src/mainMenuAreejVickie/GuiLoadingVickie.java
@@ -1,14 +1,54 @@
 package mainMenuAreejVickie;
 
+import java.awt.Font;
+import java.io.File;
+
 import guiTeacher.GUIApplication;
+import guiTeacher.components.StyledComponent;
 
 public class GuiLoadingVickie extends GUIApplication{
 
-	public GuiLoadingVickie() {
+	public GuiLoadingVickie(int width, int height) {
+		super(width, height);
+		setVisible(true);
+	}
+
+	public static GuiLoadingVickie loading;
+	public static WelcomeScreenVickie  windowOne;
+	public static MainScreenAreej windowTwo;
+
+	public static void main(String[] args) {
+		loading = new GuiLoadingVickie(1200, 800);
+		 Thread runner = new Thread(loading);
+		 runner.start();
+	}
+
+	@Override
+	public void initScreen() {
+		//StyledComponent.setTextColor(Color.white);
+		try {
+
+			 File fontFile = new File("resources//christmaseve.ttf");
+
+			 // File fontFile = new File("resources//DayRoman.ttf");
+
+			 Font font = Font.createFont(Font.TRUETYPE_FONT, fontFile);
+
+			 Font baseFont=font.deriveFont(20f);
+
+			 StyledComponent.setBaseFont(baseFont);
+
+			 } catch (Exception e) {
+
+			 e.printStackTrace();
+
+			 }
 		
-		//3 screens stephKev
-		//2 screens JessJi
-		//2 or 3 screens AnnieJoanna
+		windowOne = new WelcomeScreenVickie(getWidth(), getHeight());
+		windowTwo = new MainScreenAreej(getWidth(), getHeight());
+		setScreen(windowOne);
+		
+		//setScreen(windowTwo);
 	}
 
 }
--- src/mainMenuAreejVickie/WelcomeScreenVickie.java
+++ src/mainMenuAreejVickie/WelcomeScreenVickie.java
@@ -1,18 +1,51 @@
 package mainMenuAreejVickie;
 
+import java.awt.Color;
+import java.util.List;
+
+import guiTeacher.components.Action;
+import guiTeacher.components.Button;
+import guiTeacher.components.Graphic;
+import guiTeacher.interfaces.Visible;
+import guiTeacher.userInterfaces.FullFunctionScreen;
+
 //EDIT HIERARCHY!!
 
 public class WelcomeScreenVickie { //possibly extends clickable screen
 
-	public static void main(String[] args) {
+	public class FirstScreen extends FullFunctionScreen{
 
+		//private Button add;
+		
+		private Button switchScreen;
+		
+		public FirstScreen(int width, int height) {
+			super(width, height);
+		}
+
+		@Override
+		public void initAllObjects(List<Visible> viewObjects) {
+			//Graphic forest = new Graphic(0,0, getWidth(), getHeight(), "resources/download.jpg");
+			Graphic baymax = new Graphic(0,0, getWidth(), getHeight(), "resources/baymax.jpg");
+			
+			
+			switchScreen = new Button(350, 580, 70, 30, "Page 2", Color.red, new Action() {
+				public void act() {
+					
+					GuiLoadingVickie.loading.setScreen(GuiLoadingVickie.windowTwo);
+
+				}
+			});
+			
+			switchScreen.setCustomTextColor(new Color(250,250,250));
+			viewObjects.add(baymax);
+			//viewObjects.add(add);
+			
+			viewObjects.add(switchScreen);
+			
+			//
+			 
+			
+		}
 	}
-	/*
-	 * Make animations
-	 * 
-	 */
-
-	/*
-	 * Button/ Click on image and it goes to menu screen (.setscreen).
-	 */
 }
