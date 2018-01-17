a880b3a2b1e665bfbdf486fb8107511c078c111c
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2018-01-17 15:28:05 -0500

    V

--- src/mainMenuAreejVickie/WelcomeScreenVickie.java
+++ src/mainMenuAreejVickie/WelcomeScreenVickie.java
@@ -1,11 +1,14 @@
 package mainMenuAreejVickie;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.io.File;
 import java.util.List;
 
 import guiTeacher.components.Action;
 import guiTeacher.components.Button;
 import guiTeacher.components.Graphic;
+import guiTeacher.components.StyledComponent;
 import guiTeacher.components.TextArea;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
@@ -15,7 +18,7 @@ import guiTeacher.userInterfaces.FullFunctionScreen;
 public class WelcomeScreenVickie extends FullFunctionScreen{ //possibly extends clickable screen
 		//private Button add;
 		
-		private Button menu;
+		private  Button menu;
 		private TextArea carnival;
 
 		public WelcomeScreenVickie(int width, int height) {
@@ -27,7 +30,10 @@ public class WelcomeScreenVickie extends FullFunctionScreen{ //possibly extends 
 	
 			Graphic welcomeBackground = new Graphic(0,0, getWidth(), getHeight(), "resources/welcomeBackground.png");
 			
-			carnival = new TextArea(400,400, 500,300, "Carnival");
+			carnival = new TextArea(225,75, 1000,1000, "Carnival");
+			carnival.setForeground(Color.white);
+			carnival.update();
+//********	Changed TextArea in guiDoc.	
 			
 			menu = new Button(900, 650, 200, 100, "Welcome", Color.red, new Action() {
 				public void act() {
@@ -36,13 +42,31 @@ public class WelcomeScreenVickie extends FullFunctionScreen{ //possibly extends 
 
 				}
 			});
+			menu.setForeground(Color.orange);
+			menu.update();
+			
+			try {
+				 File fontFile = new File("resources/Bangers.ttf");
+				 Font font = Font.createFont(Font.TRUETYPE_FONT, fontFile);
+				 
+				 Font carnivalTextArea=font.deriveFont(200f);
+				 Font menuButton=font.deriveFont(40f);
+				
+				carnival.setFont(carnivalTextArea);
+				menu.setFont(menuButton); //make text orange
+				
+				 } catch (Exception e) {
+
+				 e.printStackTrace();
+
+				 }
 			
 			menu.setCustomTextColor(new Color(0,0,0)); //?
 			
-			viewObjects.add(carnival);
+			
 			viewObjects.add(welcomeBackground);
 			//viewObjects.add(add);
-			
+			viewObjects.add(carnival);
 			viewObjects.add(menu);
 			
 			//
@@ -50,5 +74,7 @@ public class WelcomeScreenVickie extends FullFunctionScreen{ //possibly extends 
 			
 		}
 		//maybe have balloons animation
+
+		
 	}
 
