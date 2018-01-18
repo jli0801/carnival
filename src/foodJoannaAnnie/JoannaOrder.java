d96fcf9eee09b7790b43e00e97409b29d0a8d1ae
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2018-01-18 15:22:23 -0500

    f

--- src/foodJoannaAnnie/JoannaOrder.java
+++ src/foodJoannaAnnie/JoannaOrder.java
@@ -19,49 +19,49 @@ public class JoannaOrder extends ClickableGraphic {
 		annie = screen;
 		numIng= (int) ((Math.random()*5)+1);
 		requested = new AnnieFoodItem[numIng];
-		
+
 		generateToppings(annie.getToppings());
 	}
- 
+
 	private AnnieFoodItem[] generateToppings(AnnieFoodItem[] arr) {
 		int a;
 		AnnieFoodItem b;
-	    for (int i = arr.length - 1; i > 0; i--) {
-	        a = (int) Math.floor(Math.random() * (i + 1));
-	        b = arr[i];
-	        arr[i] = arr[a];
-	        arr[a] = b;
-	    }
-	    for(int i = 0; i< requested.length;i++) {
-	    	requested[i]= arr[1];
-	    }
-	    
-	    return requested;
-	    
+		for (int i = arr.length - 1; i > 0; i--) {
+			a = (int) Math.floor(Math.random() * (i + 1));
+			b = arr[i];
+			arr[i] = arr[a];
+			arr[a] = b;
+		}
+		for(int i = 0; i< requested.length;i++) {
+			requested[i]= arr[1];
+		}
+
+		return requested;
+
 	}
-	
-	
+
+
 	private Object[][] getOrder(){
 		order = new Object[requested.length][2];
-	int quantity;
-	for(int aOrder =0; aOrder<order.length;aOrder++) {
-		for(int idx =0; order[aOrder].length< 2;idx++) {
-			quantity = (int) ((Math.random()*7)+1);
-			if(idx == 0) {
-				order[aOrder][idx]= quantity;
-			}else {
-				order[aOrder][idx]= requested[aOrder];
+		int quantity;
+		for(int aOrder =0; aOrder<order.length;aOrder++) {
+			for(int idx =0; order[aOrder].length< 2;idx++) {
+				quantity = (int) ((Math.random()*7)+1);
+				if(idx == 0) {
+					order[aOrder][idx]= quantity;
+				}else {
+					order[aOrder][idx]= requested[aOrder];
+				}
+
 			}
-			
+
 		}
-	
+		return order;
+
+
 	}
-	return order;
-	
-		
-	}
-	
-	
+
+
 }
 
 
