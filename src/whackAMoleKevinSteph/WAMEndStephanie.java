b3d2c27a74475cc4bc6aa2711c7f650202d9af68
BT_1N3_10 <BT_1N3_10@BT_1N3_10.bktech.local>
2018-01-30 14:46:55 -0500

    dsd

---
+++ src/whackAMoleKevinSteph/WAMEndStephanie.java
@@ -0,0 +1,21 @@
+package whackAMoleKevinSteph;
+
+import java.util.List;
+
+import guiTeacher.interfaces.Visible;
+import guiTeacher.userInterfaces.FullFunctionScreen;
+
+public class WAMEndStephanie extends FullFunctionScreen {
+
+	public WAMEndStephanie(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> viewObjects) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/whackAMoleKevinSteph/WAMGameKevin.java
+++ src/whackAMoleKevinSteph/WAMGameKevin.java
@@ -9,7 +9,7 @@ import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.ClickableScreen;
 import guiTeacher.userInterfaces.FullFunctionScreen;
 
-public class WAMGameKevin extends ClickableScreen {
+public class WAMGameKevin extends FullFunctionScreen {
 
 	private Button moleOne;
 	private Button moleTwo;
@@ -33,7 +33,7 @@ public class WAMGameKevin extends ClickableScreen {
 		//Graphic background = new Graphic(0,0, getWidth(), getHeight(), "resources/background.jpg");
 		//viewObjects.add(background);
 		
-		quitButton = new Button(100,30,210,50,"Quit Game", new Action() {
+		quitButton = new Button(100,30,100,70,"Quit Game", new Action() {
 			
 			@Override
 			public void act() {
@@ -43,7 +43,7 @@ public class WAMGameKevin extends ClickableScreen {
 		});
 		viewObjects.add(quitButton);
 		
-		moleOne = new Button(300,200,80,80,"Juan", new Action() {
+		moleOne = new Button(250,200,80,80,"One", new Action() {
 			
 			@Override
 			public void act() {
@@ -53,7 +53,7 @@ public class WAMGameKevin extends ClickableScreen {
 		});
 		viewObjects.add(moleOne);
 		
-		moleTwo = new Button(400,200,80,80,"Who", new Action() {
+		moleTwo = new Button(450,200,80,80,"Two", new Action() {
 			
 			@Override
 			public void act() {
@@ -63,7 +63,7 @@ public class WAMGameKevin extends ClickableScreen {
 		});
 		viewObjects.add(moleTwo);
 		
-		moleThree = new Button(500,200,80,80,"Tree", new Action() {
+		moleThree = new Button(650,200,80,80,"Three", new Action() {
 			
 			@Override
 			public void act() {
@@ -73,8 +73,68 @@ public class WAMGameKevin extends ClickableScreen {
 		});
 		viewObjects.add(moleThree);
 		
+		moleFour = new Button(250,400,80,80,"Four", new Action() {
+			
+			@Override
+			public void act() {
+				moleDown(4);
+				
+			}
+		});
+		viewObjects.add(moleFour);
+		
+		moleFive = new Button(450,400,80,80,"Five", new Action() {
+			
+			@Override
+			public void act() {
+				moleDown(5);
+				
+			}
+		});
+		viewObjects.add(moleFive);
+		
+		moleSix = new Button(650,400,80,80,"Six", new Action() {
+			
+			@Override
+			public void act() {
+				moleDown(6);
+				
+			}
+		});
+		viewObjects.add(moleSix);
+		
+		moleSeven = new Button(250,600,80,80,"Seven", new Action() {
+			
+			@Override
+			public void act() {
+				moleDown(7);
+				
+			}
+		});
+		viewObjects.add(moleSeven);
+		
+		moleEight = new Button(450,600,80,80,"Eight", new Action() {
+			
+			@Override
+			public void act() {
+				moleDown(8);
+				
+			}
+		});
+		viewObjects.add(moleEight);
+		
+		moleNine = new Button(650,600,80,80,"Nine", new Action() {
+			
+			@Override
+			public void act() {
+				moleDown(9);
+				
+			}
+		});
+		viewObjects.add(moleNine);
+		
 	}
-
+	
 	public void moleDown(int moleNumber) {
 		//disable something
 		
