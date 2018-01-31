70c8aea05e037dde323473ba942b1c962e66ae8b
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2018-01-31 15:23:15 -0500

    s

--- src/foodJoannaAnnie/JoannaOrder.java
+++ src/foodJoannaAnnie/JoannaOrder.java
@@ -1,5 +1,6 @@
 package foodJoannaAnnie;
 
+import java.awt.Component;
 import java.util.ArrayList;
 
 import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Collections;
@@ -8,23 +9,35 @@ import guiTeacher.components.ClickableGraphic;
 
 public class JoannaOrder extends ClickableGraphic {
 
-	private AnnieFoodItem[] requested; //toppings that will be used
-	private int numIng; //# of toppings for tht order
+	private int[] quantity; //toppings that will be used
+	private int numInt; //# of toppings for tht order
 	private AnnieWorkScreen annie;
-	private Object[][] order; //complete order
+	private AnnieFoodItem[] items; //complete order
+	private Object[][] order;
 
 
 	public JoannaOrder(int x, int y, int w, int h, String imageLocation, AnnieWorkScreen screen) {
 		super(x, y, w, h, imageLocation);
 		annie = screen;
-		numIng= (int) ((Math.random()*5)+1);
-		requested = new AnnieFoodItem[numIng];
+		numInt= (int) ((Math.random()*5)+1);
+
+		fillQuantity();
 		generateToppings(annie.getToppings());
-		order = new Object[requested.length][2];
-		getOrder();
+		makeOrder();
+	
 	}
 
-	private AnnieFoodItem[] generateToppings(AnnieFoodItem[] arr) {
+	private void makeOrder() {
+		order = new Object[numInt][2];
+		for(int i =0; i<items.length; i++) {
+			order[i][0] = quantity[i];
+			order[i][1] = items[i];
+		}
+		
+	}
+
+	private void generateToppings(AnnieFoodItem[] arr) {
+		items=  new AnnieFoodItem[numInt];
 		int a;
 		AnnieFoodItem b;
 		for (int i = arr.length - 1; i > 0; i--) {
@@ -33,47 +46,31 @@ public class JoannaOrder extends ClickableGraphic {
 			arr[i] = arr[a];
 			arr[a] = b;
 		}
-		for(int i = 0; i< requested.length;i++) {
-			requested[i]= arr[1];
+		for(int i = 0; i< items.length;i++) {
+			items[i]= arr[i];
 		}
-
-		return requested;
 
 	}
 
-
-	private Object[][] getOrder(){
-
-		int quantity;
-		for(int aOrder =0; aOrder<order.length;aOrder++) {
-			for(int idx =0; order[aOrder].length< 2;idx++) {
-				quantity = (int) ((Math.random()*7)+3);
-				if(idx == 0) {
-					order[aOrder][idx]= quantity;
-				}else {
-					order[aOrder][idx]= requested[aOrder]; 
-
-				}
-
-			}
-
-		}
-		return order;
+	private void fillQuantity() {
+		quantity = new int[numInt];
+		for(int i =0; i < quantity.length; i++) {
+			quantity[i] = (int)((Math.random()*8)+4);
+		}	
 	}
+
 
 	public String toString() {
 		String s="1 LARGE PIZZA" +"\n";
-		for(int i=0; i<order.length; i++) {
-
-			for(int j=0; j<order[i].length-1; j++) {
-				s+="-"+ order[i][j]+  "" + order[i][j+1]+"\n"; 
+				for(int i=0; i<numInt; i++) {
+						s+="- "+ order[i][0]+  " " + ((AnnieFoodItem) order[i][1]).getName()+"\n"; 
+				}
+				return s;
 			}
-		}
-		return s;
+
+
 	}
 
-
-}
 
 
 
