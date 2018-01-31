1b26dc4ad32c1e2dae97ce3a4eb002d5a6cc9b54
BT_1N3_10 <BT_1N3_10@BT_1N3_10.bktech.local>
2018-01-31 15:25:56 -0500

    RR

--- src/whackAMoleKevinSteph/WAMEndStephanie.java
+++ src/whackAMoleKevinSteph/WAMEndStephanie.java
@@ -8,6 +8,7 @@ import java.util.List;
 import guiTeacher.components.Action;
 import guiTeacher.components.Button;
 import guiTeacher.components.StyledComponent;
+import guiTeacher.components.TextBox;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
 import mainMenuAreejVickie.GuiLoadingVickie;
@@ -16,6 +17,9 @@ public class WAMEndStephanie extends FullFunctionScreen {
 
 	private Button returnMSButton;
 	private Button playButton;
+	private TextBox resultTitle;
+	private TextBox showScores;
+	private TextBox totalTickets;
 	
 	public WAMEndStephanie(int width, int height) {
 		super(width, height);
@@ -38,22 +42,33 @@ public class WAMEndStephanie extends FullFunctionScreen {
 
 			}
 		 
-		returnMSButton = new Button(180,30,210,50,"RETURN TO MAIN MENU",Color.ORANGE, new Action() {
+		returnMSButton = new Button(30,30,210,50,"RETURN TO MAIN MENU",Color.ORANGE, new Action() {
 
 				public void act() {
 					GuiLoadingVickie.loading.setScreen(GuiLoadingVickie.menu);
 				}
 			});
 
-			viewObjects.add(returnMSButton);
+		viewObjects.add(returnMSButton);
 		 
-		playButton = new Button(180,30,500,50,"PLAY AGAIN", Color.ORANGE, new Action() {
+		playButton = new Button(780,30,500,50,"PLAY AGAIN", Color.ORANGE, new Action() {
 			
 			public void act() {
 				GuiLoadingVickie.loading.setScreen(GuiLoadingVickie.moleInstruct);
 			}
 		});
-
+		
+		viewObjects.add(playButton);
+		
+		resultTitle = new TextBox(250,80,700,270,"RESULTS:");
+		viewObjects.add(resultTitle);
+		
+		showScores = new TextBox(250,80,700,270,"SCORE:" + "\n" + "TICKETS EARNED:");
+		viewObjects.add(showScores);
+		
+		totalTickets = new TextBox(250,80,700,270,"TOTAL TICKETS:");
+		viewObjects.add(totalTickets);
+		
 	}
 
 }
