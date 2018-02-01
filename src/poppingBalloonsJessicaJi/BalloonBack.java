7468c5dae718135633e154ae4dbc2616f329e729
BT_1N3_08 <BT_1N3_08@BT_1N3_08.bktech.local>
2018-02-01 14:31:27 -0500

    darts

--- src/poppingBalloonsJessicaJi/BalloonBack.java
+++ src/poppingBalloonsJessicaJi/BalloonBack.java
@@ -9,7 +9,7 @@ public class BalloonBack {
 //	public static ArrayList<String> darts = new ArrayList<String>();  //static b/c jessica needs to use it 
 //	public static ArrayList<Integer> dartsProperty = new ArrayList<Integer>(); //static b/c jessica needs to use it 
 	public static ArrayList<Integer> balloons = new ArrayList<Integer>(); //static b/c jessica needs to use it index tells us what color what's in the index is the strength of balloon
-
+	public static ArrayList<Integer> darts = Inventory.getDarts();
 //	public static ArrayList<Integer> balloonsProperty = new ArrayList<Integer>(); //static b/c jessica needs to use it
 	
 	public void Timer() { //links to text area
@@ -28,15 +28,14 @@ public class BalloonBack {
 		});
 	}
 	
-	public static ArrayList<String> CreateDarts() {
+	public static ArrayList<Integer> CreateDarts() {
 		int numDarts = 10;
 		for(int i = 0; i < numDarts; i++) {
 			if(i <= 4 ) {
-				darts.add(Integer.toString(i));
-				dartsProperty.add(0); //blunt
+				
+				darts.add(i, 0); //blunt
 			}else {
-				darts.add(Integer.toString(i));
-				dartsProperty.add(1); //weak
+				darts.add(i, 1); //weak
 			}
 			
 		}		
@@ -50,7 +49,7 @@ public class BalloonBack {
 		//for loop make balloons
 		int numBalloons = 6;
 		for(int i = 0; i < numBalloons; i++) {
-			balloons.add(0); // all start w/ weak
+			balloons.add(i,0); // all start w/ weak
 		//	balloonsProperty.add(0); //starts out with 0		
 			}		
 		
@@ -63,7 +62,7 @@ public class BalloonBack {
 		//thread for time & disable button  
 		//change a balloon at the same position and change strength 
 		
-		if(balloonsProperty.get(x) == (dartsProperty.get(dartProperty)) || dartsProperty.get(dartProperty) > balloonsProperty.get(x)) 
+		if(balloons.get(x) == (darts.get(dartProperty)) || darts.get(dartProperty) > balloons.get(x)) 
 		{
 			//same property/type/level
 			balloons.remove(x);
