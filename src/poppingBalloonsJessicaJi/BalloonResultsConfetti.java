797a43baacaa9cadc219520639d8aa70e2c8473a
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2018-02-01 15:10:32 -0500

    ballooon results class animation

---
+++ src/poppingBalloonsJessicaJi/BalloonResultsConfetti.java
@@ -0,0 +1,21 @@
+package poppingBalloonsJessicaJi;
+
+import java.awt.Graphics2D;
+
+import guiTeacher.components.AnimatedComponent;
+
+public class BalloonResultsConfetti extends AnimatedComponent{
+
+	public BalloonResultsConfetti() {
+		super(40, 40, 100, 80);
+		addSequence("resources/", 150, 0, 0, 100, 200, 10);
+		Thread animation = new Thread(this);
+		animation.start();
+		update();
+	}
+
+	@Override
+	public void update(Graphics2D g) {
+		super.update(g);
+	}
+}
