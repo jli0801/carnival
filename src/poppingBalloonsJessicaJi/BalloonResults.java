cafa5e49cbc5d5ddec8d091d9f9617b835406012
BT_1N3_10 <BT_1N3_10@BT_1N3_10.bktech.local>
2018-02-06 15:11:00 -0500

    x

--- src/poppingBalloonsJessicaJi/BalloonResults.java
+++ src/poppingBalloonsJessicaJi/BalloonResults.java
@@ -13,7 +13,7 @@ import mainMenuAreejVickie.GuiLoadingVickie;
 
 public class BalloonResults extends FullFunctionScreen {
 
-	public static poppingBalloonsJessicaJi.BalloonScreen balloonScreen;
+	//public static poppingBalloonsJessicaJi.BalloonScreen balloonScreen;
 	//private TextArea result;
 	private TextArea score;
 	//private TextArea rate;
@@ -31,7 +31,7 @@ public class BalloonResults extends FullFunctionScreen {
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-		balloonScreen = new poppingBalloonsJessicaJi.BalloonScreen(getWidth(), getHeight());
+		//balloonScreen = new poppingBalloonsJessicaJi.BalloonScreen(getWidth(), getHeight());
 		//result = new TextArea(50, 10, 200, 100, "You (win/lose)");
 		score = new TextArea(200, 100, 100, 50, "Final score: " + scoreNum);
 		//rate  = new TextArea(50, 70, 500, 50, "You popped an average of _ balloons per second");
@@ -52,7 +52,7 @@ public class BalloonResults extends FullFunctionScreen {
 			
 			@Override
 			public void act() {
-				GuiLoadingVickie.loading.setScreen(balloonScreen);
+				//GuiLoadingVickie.loading.setScreen(balloonScreen);
 			}
 		});
 		
