4aedc402e3c8bbe729f4c6b3901a45f20089ee1f
Jessica Li <jli0801@bths.edu>
2018-02-06 13:59:30 -0500

    fixed formula

--- src/poppingBalloonsJessicaJi/PowerBar.java
+++ src/poppingBalloonsJessicaJi/PowerBar.java
@@ -48,16 +48,16 @@ public class PowerBar extends StyledComponent {
 			g.fillRect(0, 0, getWidth(), getHeight()); 
 			if(goingUp) //going up
 			{
-				length = length + 25; // 5,10,15,20,25,30,35,40
+				length = length + 25; // 150,175,200,225,250,275,300,325,350,375,400,425,450,475,500
 				if(length <= 250 && length > 150) // length is between 50 to 200
 				{
 					g.setColor(lightYellow);
 				}
-				else if (length <= 325 && length > 250) //16 -> 25
+				else if (length <= 350 && length > 250) //16 -> 25
 				{
 					g.setColor(Color.YELLOW);
 				}
-				else if (length <= 325 && length > 350) //26 -> 35
+				else if (length <= 425 && length > 350) //26 -> 35
 				{
 					g.setColor(Color.ORANGE);
 				}
@@ -66,23 +66,23 @@ public class PowerBar extends StyledComponent {
 					g.setColor(Color.RED);
 				}
 				
-				if(length == 400)
+				if(length == 500)
 				{
 					goingUp = false;
 				}
 			}
 			else //going down 
 			{
-				length = length - 25; //5,10,15,20,25,30,35,40
-				if(length >= 400 && length < 375) //36 -> 40
+				length = length - 25; // 150,175,200,225,250,275,300,325,350,375,400,425,450,475,500
+				if(length >= 500 && length < 425) //36 -> 40
 				{
 					g.setColor(Color.RED);
 				}
-				else if (length >= 300 && length < 375) //35 ->26
+				else if (length >= 425 && length < 350) //35 ->26
 				{
 					g.setColor(Color.ORANGE);
 				}
-				else if (length >= 200 && length < 300) //25 -> 16
+				else if (length >= 350 && length < 250) //25 -> 16
 				{
 					g.setColor(Color.YELLOW);
 				}
