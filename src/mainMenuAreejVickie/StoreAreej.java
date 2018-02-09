a79d0998d60543d2e69877d9bdf696f8dea2df46
Family <Family@LAPTOP-TU0DEQG3>
2018-02-08 23:10:57 -0500

    store update

--- src/mainMenuAreejVickie/StoreAreej.java
+++ src/mainMenuAreejVickie/StoreAreej.java
@@ -6,6 +6,8 @@ import java.util.List;
 import guiTeacher.components.Action;
 import guiTeacher.components.Button;
 import guiTeacher.components.Graphic;
+import guiTeacher.components.TextArea;
+import guiTeacher.components.TextLabel;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
 
@@ -14,6 +16,12 @@ public class StoreAreej extends FullFunctionScreen{
 	private Button menu;
 	private Button inventory;
 	
+	private static double money = 10;
+	private static int ticket; 
+	
+	
+	private TextArea ticketDisplay;
+	private TextArea moneyDisplay;
 	
 	public StoreAreej(int width, int height) {
 		super(width, height);
@@ -24,9 +32,19 @@ public class StoreAreej extends FullFunctionScreen{
 	public void initAllObjects(List<Visible> viewObjects) {
 		
 		Graphic background = new Graphic(0,0, 1185, getHeight(), "resources/tent.png");
-		Graphic bear = new Graphic(10,10, 100, 100, "resources/bear.png");
+		Graphic shelf = new Graphic(330,275, 800, 700, "resources/shelf.png");
+		Graphic bear = new Graphic(510,400, 100, 100, "resources/bear.png");
+		Graphic fish = new Graphic(670,400, 100, 100, "resources/fish.png");
+		Graphic candy = new Graphic(830,400, 100, 100, "resources/cottoncandy.png");
+		Graphic dull = new Graphic (415,275,130,130, "poppingBalloons/dart1.png"); 
+		Graphic blunt = new Graphic (590,275,130,130, "poppingBalloons/dart2.png"); 
+		Graphic sharp = new Graphic (770,275,130,130, "poppingBalloons/dart3.png"); 
+		Graphic dangerous = new Graphic (920,275,130,130, "poppingBalloons/dart4.png"); 
+		Graphic ticket = new Graphic (30, 200, 100, 100, "resources/ticket.jpg");
+		Graphic money = new Graphic(30, 290, 90, 90, "resources/money.png");
+		Graphic hammer = new Graphic(670, 520, 100, 100, "resources/hammer.png");
 		
-		menu = new Button(10, 600, 50, 50, "menu", Color.blue, new Action() {
+		menu = new Button(30, 580, 60, 50, "menu", Color.blue, new Action() {
 
 			@Override
 			public void act() {
@@ -35,7 +53,7 @@ public class StoreAreej extends FullFunctionScreen{
 			
 		});
 		
-		inventory = new Button(10,700, 100, 50, "inventory", Color.orange, new Action() {
+		inventory = new Button(30,660, 100, 50, "inventory", Color.orange, new Action() {
 
 			@Override
 			public void act() {
@@ -44,13 +62,46 @@ public class StoreAreej extends FullFunctionScreen{
 			
 		});
 		
+		TextLabel store = new TextLabel(730,180, 900, 300, "STORE");
+		
+		moneyDisplay = new TextArea(10, 200, 100, 30, "10");
+		ticketDisplay = new TextArea(10, 290, 100, 30, "0");
+		
 		viewObjects.add(background);
+		viewObjects.add(store);
+		viewObjects.add(shelf);
 		viewObjects.add(bear);
+		viewObjects.add(dull);
+		viewObjects.add(blunt);
+		viewObjects.add(sharp);
+		viewObjects.add(dangerous);
+		viewObjects.add(fish);
+		viewObjects.add(candy);
+		viewObjects.add(ticket);
+		viewObjects.add(money);
+		viewObjects.add(hammer);
 		viewObjects.add(menu);
 		viewObjects.add(inventory);
 		
+		viewObjects.add(moneyDisplay);
+		viewObjects.add(ticketDisplay);
+		
+		
 	}
-
 	
+	public void addMoney() {
+		money++;
+	//	moneyDisplay.setText(money);
+		
+	}
+	
+	public void addTickets() {
+		ticket++;
+	//	ticketDisplay.setText(tickets);
+		
+		
+	}
+	
+		
 
 }
