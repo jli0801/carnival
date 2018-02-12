ce329533c416e4a8d6436b02df694462a4727853
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2018-02-12 14:20:45 -0500

    dart class done

---
+++ src/poppingBalloonsJessicaJi/DartJessica.java
@@ -0,0 +1,75 @@
+package poppingBalloonsJessicaJi;
+
+import java.awt.Color;
+
+public class DartJessica {
+
+	private String type;
+	private int strength; //dull = 1 blunt = 2 
+	private Color color;
+	
+	public DartJessica(String kind) {
+		type = kind;
+		strength = setStrength(kind);
+		color = setColor(kind);
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public int getStrength() {
+		return strength;
+	}
+	
+	public int setStrength(String kind) {
+		if(kind.equals("Dull"))
+		{
+			return 1;
+		}
+		else if(kind.equals("Blunt"))
+		{
+			return 2;
+		}
+		else if(kind.equals("Strong"))
+		{
+			return 3;
+		}
+		else
+		{
+			return 4;
+		}
+	}
+	
+	//darts.get(i).getStrength = 1,2,3,4 
+	
+	public Color getColor() {
+		return color;
+	}
+	
+	public Color setColor(String kind) {
+		if(kind.equals("Dull"))
+		{
+			return Color.BLACK;
+		}
+		else if(kind.equals("Blunt"))
+		{
+			return Color.BLUE;
+		}
+		else if(kind.equals("Strong"))
+		{
+			return Color.GREEN;
+		}
+		else
+		{
+			return Color.RED;
+		}
+	}
+	
+	
+
+}
