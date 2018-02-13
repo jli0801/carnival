73754e41cdc9917cf3b82ddb236a85a6c6c14663
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2018-02-13 15:21:53 -0500

    o

---
+++ src/whackAMoleKevinSteph/WAMCasual.java
@@ -0,0 +1,15 @@
+package whackAMoleKevinSteph;
+
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import guiTeacher.interfaces.Visible;
+
+public class WAMCasual extends WAMEasyKevin {
+	
+	public WAMCasual(int width, int height) {
+		super(width, height);
+	}
+	
+}
