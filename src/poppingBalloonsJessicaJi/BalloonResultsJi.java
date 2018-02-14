12e054cf2e17378a0c3aa7402b5f5b9a4294192b
BT_1N3_10 <BT_1N3_10@BT_1N3_10.bktech.local>
2018-02-14 15:15:45 -0500

    :D

--- src/poppingBalloonsJessicaJi/BalloonResultsJi.java
+++ src/poppingBalloonsJessicaJi/BalloonResultsJi.java
@@ -50,6 +50,7 @@ public class BalloonResultsJi extends FullFunctionScreen {
 		 */
 		calculateTickets();
 		ticketTxt.setText("Tickets Earned: " + tickets);
+		//addTicketsInventory();
 	}
 
 	@Override
@@ -140,6 +141,12 @@ public class BalloonResultsJi extends FullFunctionScreen {
 
 	public void calculateTickets() {
 		tickets = (int) (scoreNum/2);
+	}
+	
+	public void addTicketsInventory() {
+		int t = mainMenuAreejVickie.InventoryVickie.getTickets();
+		int totalTickets = t + tickets;
+		//mainMenuAreejVickie.InventoryVickie.setTickets(totalTickets);
 	}
 	
 	public String checkSecondPlace() {
