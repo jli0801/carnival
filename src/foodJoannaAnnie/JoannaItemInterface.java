fb1f2ea515780412ecc69c969847679ba225d262
joann <joann@DESKTOP-56VO8EV>
2018-02-14 19:49:42 -0500

    interfaces

---
+++ src/foodJoannaAnnie/JoannaItemInterface.java
@@ -0,0 +1,13 @@
+package foodJoannaAnnie;
+
+public interface JoannaItemInterface {
+	
+	public String getName();
+	
+	public double getPrice();
+	
+	public boolean isTopping();
+		
+	public boolean equals(AnnieFoodItem item);
+
+}
