29b6c0d038369b6c5a8b88434af0598b0758f5ab
yejaz <yejaz@192.168.0.14>
2018-02-16 00:15:23 -0500

    b

---
+++ src/poppingBalloonsJessicaJi/PowerBarJiInterface.java
@@ -0,0 +1,12 @@
+package poppingBalloonsJessicaJi;
+
+import java.awt.Graphics2D;
+
+public interface PowerBarJiInterface {
+
+	int getLength();
+	
+	void update(Graphics2D g);
+	
+	void startTask();
+}
